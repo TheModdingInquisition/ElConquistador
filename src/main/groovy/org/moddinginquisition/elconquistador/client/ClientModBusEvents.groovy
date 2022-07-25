1f3e7201a95182c64f3ef16779ee76908e672267
Paint_Ninja <PaintNinja@users.noreply.github.com>
2022-07-25 14:58:31 +0100

    Add config screen

---
+++ src/main/groovy/org/moddinginquisition/elconquistador/client/ClientModBusEvents.groovy
@@ -0,0 +1,83 @@
+package org.moddinginquisition.elconquistador.client
+
+import ga.ozli.minecraftmods.groovylicious.api.gui.Alignment
+import ga.ozli.minecraftmods.groovylicious.api.gui.ExtensibleScreen
+import ga.ozli.minecraftmods.groovylicious.dsl.ScreenBuilder
+import groovy.transform.CompileStatic
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.screens.Screen
+import net.minecraft.client.gui.screens.TitleScreen
+import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.client.ConfigScreenHandler
+import net.minecraftforge.client.gui.ModListScreen
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.ModLoadingContext
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent
+import net.minecraftforge.fml.loading.FMLPaths
+import net.thesilkminer.mc.austin.api.EventBus
+import net.thesilkminer.mc.austin.api.EventBusSubscriber
+import org.moddinginquisition.elconquistador.Configs
+
+import static org.moddinginquisition.elconquistador.ElConquistador.LOGGER
+import static org.moddinginquisition.elconquistador.ElConquistador.MOD_ID
+
+@CompileStatic
+@EventBusSubscriber(modId = MOD_ID, bus = EventBus.MOD, dist = Dist.CLIENT)
+class ClientModBusEvents {
+
+    static Screen returnTo = new TitleScreen()
+    static final ExtensibleScreen configScreen = ScreenBuilder.makeScreen('ElConquistador Config Screen') {
+        drawBackground = true
+
+        label {
+            text 'ElConquistador Config Screen'
+            position x: 0, y: 20
+            alignment Alignment.CENTRE
+        }
+
+        button {
+            text 'Open common config'
+            position(50, 50)
+            size(150, 20)
+            onPress {
+                configScreen.openFile(new File(FMLPaths.CONFIGDIR.get().toString(), "$MOD_ID-common.toml"))
+            }
+        }
+
+        button {
+            text 'Open client config'
+            position(50, 80)
+            size(150, 20)
+            onPress {
+                // todo: support Configs.Client.configFile in Groovylicious
+                configScreen.openFile(new File(FMLPaths.CONFIGDIR.get().toString(), "$MOD_ID-client.toml"))
+            }
+        }
+
+        button {
+            text 'Close'
+            position {
+                x = 50
+                y = 120
+            }
+            size {
+                width = 150
+                height = 20
+            }
+            onPress {
+                Minecraft.instance.screen = returnTo
+            }
+        }
+    }
+
+    @SubscribeEvent
+    static void onModConstruction(final FMLConstructModEvent event) {
+        ModLoadingContext.get().registerExtensionPoint(
+                ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory((Minecraft mc, Screen returnTo) -> {
+                    this.returnTo = returnTo
+                    return configScreen
+                })
+        )
+    }
+}
--- src/main/groovy/org/moddinginquisition/elconquistador/client/WelcomeScreen.groovy
+++ src/main/groovy/org/moddinginquisition/elconquistador/client/WelcomeScreen.groovy
@@ -1,8 +1,13 @@
 package org.moddinginquisition.elconquistador.client
 
+import ga.ozli.minecraftmods.groovylicious.api.gui.Alignment
+import ga.ozli.minecraftmods.groovylicious.api.gui.ExtensibleScreen
+import ga.ozli.minecraftmods.groovylicious.api.gui.Position
 import ga.ozli.minecraftmods.groovylicious.dsl.ScreenBuilder
 import groovy.transform.CompileStatic
+import net.minecraft.Util
 import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.screens.ConfirmLinkScreen
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.client.gui.screens.TitleScreen
 import org.moddinginquisition.elconquistador.Configs
@@ -11,16 +16,23 @@ import static ga.ozli.minecraftmods.groovylicious.api.gui.ColoursRegistry.instan
 
 @CompileStatic
 class WelcomeScreen {
-    static Screen screen = ScreenBuilder.makeScreen('ElConquistador Welcome Screen') {
+    static final ExtensibleScreen screen = ScreenBuilder.makeScreen('ElConquistador Welcome Screen') {
         drawBackground = true
 
+        //plainTextButton {
         label {
             text 'El Conquistador installed successfully!'
             position {
-                x = 10
+                x = 50
                 y = 10
             }
-            textColour Colours.WHITE
+            size {
+                width = 200
+                height = 20
+            }
+//            onPress {
+//                screen.confirmOpenLink('https://github.com/TheModdingInquisition/ElConquistador')
+//            }
         }
 
         button {
